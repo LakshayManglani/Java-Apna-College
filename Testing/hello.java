84caf369e34d11fab57b2ab3422ecd4f87582ff0
LakshayManglani <lakshaymanglani2212@gmail.com>
2022-08-12 17:43:10 +0000

    update hello.java

--- Testing/hello.java
+++ Testing/hello.java
@@ -1,5 +1,8 @@
-public class hello{
+public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
